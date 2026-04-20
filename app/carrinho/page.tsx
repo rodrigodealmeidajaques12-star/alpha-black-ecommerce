8c931474231b5e42111009ccd36621c5922bf3c1
Rodrigo Almeida <rodrigodealmeidajaques12@gmail.com>
2026-04-20 14:24:16 -0400

    feat(cart):  implement cart with quantity control, remove item and checkout

--- app/carrinho/page.tsx
+++ app/carrinho/page.tsx
@@ -7,8 +7,41 @@ export default function Carrinho() {
 
   useEffect(() => {
     const dados = JSON.parse(localStorage.getItem("carrinho") || "[]");
+
+    const carrinhoComQuantidade = dados.map((item: any) => ({
+      ...item,
+      quantidade: item.quantidade || 1
+    }))
     setCarrinho(dados);
   }, []);
+
+  function finalizarCompra(){
+    localStorage.removeItem("carrinho"); // isso vai limpar o carrinho
+    setCarrinho([]); //atualiza o estado
+    alert ("Compra finalizada com sucesso! 🎉 ");
+  }
+
+  function aumentarQuantidade(index: number) {
+    const novoCarrinho = [...carrinho];
+    novoCarrinho[index].quantidade += 1;
+
+    setCarrinho(novoCarrinho);
+    localStorage.setItem("carrinho", JSON.stringify(novoCarrinho));
+  }
+
+  function diminuirQuantidade(index: number) {
+    const novoCarrinho = [...carrinho];
+
+    if (novoCarrinho[index].quantidade > 1) {
+      novoCarrinho[index].quantidade -= 1;
+    } else {
+      novoCarrinho.splice(index, 1); //REMOVE SE FOR 1
+    }
+
+    setCarrinho(novoCarrinho);
+    localStorage.setItem("carrinho", JSON.stringify(novoCarrinho));
+  }
+  
 
   function removerItem(index: number) {
     const novoCarrinho = [...carrinho];
@@ -44,21 +77,50 @@ export default function Carrinho() {
                 <p>R$ {item.preco} x {item.quantidade}</p>
               </div>
 
-              <button
-                onClick={() => removerItem(index)}
-                className="bg-red-500 px-4 py-2 rounded"
-              >
-                Remover
-              </button>
-            </div>
-          ))}
-        </div>
-      )}
+              <div className= "flex items-center gap-3">
+                <button
+                  onClick={() => diminuirQuantidade(index)}
+                    className="bg-red-500 px-3 py-1 rounded"
+                    >
+                    -
+                    </button>
 
-      <h2 className="mt-6 text-xl">
-        Total: R$ {total.toFixed(2)}
-      </h2>
+                        <span>{item.quantidade}</span>
 
-    </main>
-  );
+                         <button
+                           onClick={() => aumentarQuantidade(index)}
+                             className="bg-green-500 px-3 py-1 rounded"
+                              >
+                              +
+                              </button>
+
+                              </div>
+                            
+                              <button
+                             onClick={() => removerItem(index)}
+                             className="bg-red-500 px-4 py-2 rounded"
+                              >
+                              Remover
+                              </button>
+                            </div>
+
+                            
+                          ))}
+                        </div>
+                      )}
+
+                      <h2 className="mt-6 text-xl">
+                        Total: R$ {total.toFixed(2)}
+                      </h2>
+
+                      <button
+                        onClick={finalizarCompra}
+                        className="mt-6 bg-yellow-500 text-black px-6 py-3 rounded hover:bg-yellow-400"
+                        >
+                           Finalizar compra
+                          </button>
+
+                    </main>
+                  );
 }
+                
